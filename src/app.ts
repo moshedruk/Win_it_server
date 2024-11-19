38a406a9a9a9f697bbdda926c16615de82234985
Netanel Rotenstein <email@example.com>
2024-11-19 14:53:55 +0200

    app basic

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express  from "express"
+import http from 'http'
+import { Server } from "socket.io";
+import cors from 'cors'
+
+const app = express()
+
+const httpServer = http.createServer(app)
+export const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: "*",
+  },
+});
+
+
+app.use(express.json())
+app.use(cors())
+
+httpServer.listen(process.env.PORT, () => { console.log('server is running') })
